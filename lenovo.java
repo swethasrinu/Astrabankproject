8dbe35c658e547657d06221ecbbf7eec05339b71
swethasrinu <kasturiswetha201@gmail.com>
2023-01-23 14:18:06 +0530

    lenovo java file added

---
+++ lenovo.java
@@ -0,0 +1,12 @@
+package Oops.Abstract;
+
+public class lenovo extends hp{
+	
+	 public void monitor() {
+		System.out.println("monitor");
+	}
+	 public void speaker() {
+		System.out.println("speaker");
+	}
+	
+}
